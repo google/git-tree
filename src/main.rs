b33a40f5504a4c6e1e0c8976671815335e0f0200
Johnathan Van Why <jrvanwhy@google.com>
2025-01-14 21:06:10 -0800

    More refactorings and cleanups.

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,34 @@
 //! A wrapper around `git log --graph` that heuristically determines what set of
 //! commits should be displayed.
 
+// The "interesting branches" are all local branches and all remote branches
+// that are tracked by a local branch. The "interesting commits" are the commits
+// pointed to by the interesting branches plus the HEAD commit. This tool
+// displays the interesting commits, their collective merge bases, and any
+// commits on the paths between the merge bases and the interesting commits.
+
 use core::str;
 use std::collections::HashSet;
 use std::io::{BufRead as _, BufReader};
 use std::process::{Command, Stdio};
 
-#[allow(clippy::allow_attributes)] // TODO: Remove
-#[allow(clippy::allow_attributes_without_reason)] // TODO: Remove
-#[allow(clippy::print_stderr)] // TODO: Remove
-#[allow(clippy::shadow_unrelated)] // TODO: Remove
-fn main() {
+/// Returns all interesting branches. Note that some commits may be in the list
+/// multiple times under different names (different ref paths and/or a SHA
+/// hash).
+/// Precondition: `buffer` must be empty
+/// Postcondition: `buffer` will be empty
+fn interesting_branches(buffer: &mut Vec<u8>) -> HashSet<String> {
     let mut git = Command::new("git")
         .args(["branch", "--format=%(objectname)|%(upstream)"])
         .stdout(Stdio::piped())
         .spawn()
         .expect("failed to run git");
     let mut interesting = HashSet::new();
-    // Capacity estimate is a guess -- twice as large as a SHA-256 hash seems
-    // reasonable.
-    let mut part = Vec::with_capacity(128);
     let mut reader = BufReader::new(git.stdout.as_mut().unwrap());
     while let Some(len) =
-        reader.read_until(b'\n', &mut part).expect("git stdout read failed").checked_sub(1)
+        reader.read_until(b'\n', buffer).expect("git stdout read failed").checked_sub(1)
     {
-        let (id, upstream) = str::from_utf8(part.get(..len).unwrap())
+        let (id, upstream) = str::from_utf8(buffer.get(..len).unwrap())
             .expect("non-utf-8 git output")
             .split_once('|')
             .expect("incorrect branch --format output");
@@ -46,36 +50,61 @@ fn main() {
         if !upstream.is_empty() {
             interesting.insert(upstream.to_owned());
         }
-        part.clear();
+        buffer.clear();
     }
-    drop(part);
+    drop(reader);
     let status = git.wait().expect("failed to wait for git");
     assert!(status.success(), "git returned unsuccessful status {status}");
+    interesting
+}
 
-    // TODO: Spawn and stream in stdout? Skip UTF-8 checking?
-    let output = Command::new("git")
+/// Returns all merge bases of the interesting commits.
+/// Precondition: `buffer` must be empty
+/// Postcondition: `buffer` will be empty
+fn merge_bases(buffer: &mut Vec<u8>, interesting_branches: &HashSet<String>) -> Vec<String> {
+    let mut git = Command::new("git")
         .args(["merge-base", "-a", "--octopus", "HEAD"])
-        .args(&interesting)
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("Failed to run git");
-    if !output.status.success() {
-        eprintln!(
-            "Git returned an unsuccessful status: {}. Git output:\n{}",
-            output.status,
-            String::from_utf8_lossy(&output.stdout)
-        );
-        return;
+        .args(interesting_branches)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to run git");
+    let mut merge_bases = Vec::with_capacity(1);
+    let mut reader = BufReader::new(git.stdout.as_mut().unwrap());
+    while let Some(len) =
+        reader.read_until(b'\n', buffer).expect("git stdout read failed").checked_sub(1)
+    {
+        // Reserve enough space for the merge base plus a trailing ^@ (used in
+        // the final `git log --graph` invocation).
+        #[allow(
+            clippy::arithmetic_side_effects,
+            reason = "A line large enough to make this overflow does not fit in the address space"
+        )]
+        let mut merge_base = String::with_capacity(len + 2);
+        merge_base
+            .push_str(str::from_utf8(buffer.get(..len).unwrap()).expect("non-utf-8 git output"));
+        merge_bases.push(merge_base);
+        buffer.clear();
     }
-    let mut merge_bases = Vec::new();
-    for line in str::from_utf8(&output.stdout).expect("non-UTF-8 git output").lines() {
-        merge_bases.push(line);
-    }
+    let status = git.wait().expect("failed to wait for git");
+    assert!(status.success(), "git returned unsuccessful status {status}");
+    merge_bases
+}
+
+#[allow(clippy::allow_attributes_without_reason)] // TODO: Remove
+#[allow(clippy::print_stderr)] // TODO: Remove
+#[allow(clippy::shadow_unrelated)] // TODO: Remove
+fn main() {
+    // Capacity estimate is a guess -- twice as large as a SHA-256 hash seems
+    // reasonable (and is a power of two).
+    let mut buffer = Vec::with_capacity(128);
+    let interesting_branches = interesting_branches(&mut buffer);
+    let merge_bases = merge_bases(&mut buffer, &interesting_branches);
+    drop(buffer);
 
     // TODO: Stream.
     let output = Command::new("git")
-        .args(["rev-list", "--parents", "--reverse", "--topo-order"])
-        .args(&interesting)
+        .args(["rev-list", "--parents", "--reverse", "--topo-order", "HEAD"])
+        .args(&interesting_branches)
         .arg("--not")
         .args(&merge_bases)
         .stderr(Stdio::inherit())
@@ -89,7 +118,7 @@ fn main() {
         );
         return;
     }
-    let mut visible: HashSet<&str> = merge_bases.iter().copied().collect();
+    let mut visible: HashSet<&str> = merge_bases.iter().map(String::as_str).collect();
     let mut includes: HashSet<&str> = HashSet::new();
     let mut stragglers: HashSet<&str> = HashSet::new();
     for line in str::from_utf8(&output.stdout).unwrap().lines() {
